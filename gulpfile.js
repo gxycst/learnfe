5a88a2c60f3e84058a74b84c2f0ac72296574802
fetjq <596713230@qq.com>
2016-05-27 18:02:25 +0800

    20160527

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp'),
+/*var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
@@ -28,3 +28,30 @@ gulp.task('styles', function() {
     //提醒任务完成
     .pipe(notify({ message: 'Styles task complete' }));
 });
+*/
+//包含处理包
+var gulp=require('gulp'),
+    notify=require('gulp-notify'),
+    autoprefixer=require('gulp-autoprefixer'),
+    sass=require('gulp-sass');
+//创建处理任务
+
+gulp.task('cssTask',function(){
+    //用gulp.src找到路径
+    return gulp.src('./scss/index.scss')
+    //编译sass
+    .pipe(sass())
+    //添加浏览器前缀
+    .pipe(autoprefixer('last 2 version','safari 5','ie 8','ie 9','opera 12.1','ios6','android 4'))
+    //用gulp.dest保存未压缩文件到指定目录
+    .pipe(gulp.dest('maxcss'))
+    .pipe(notify({message:'cssTask is ok ^_^!!'}));
+});
+//默认任务
+gulp.task('default', function() {
+    gulp.start('cssTask');
+});
+//监听自动执行
+gulp.task('watch',function(){
+    gulp.watch('scss/*.scss','cssTask');
+})
