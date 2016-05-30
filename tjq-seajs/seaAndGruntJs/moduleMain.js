ec76fcc94dcbc230b1288c0a11fe18599716b6e4
tjq <596713230@qq.com>
2016-05-31 00:41:06 +0800

    20160531

--- tjq-seajs/seaAndGruntJs/moduleMain.js
+++ tjq-seajs/seaAndGruntJs/moduleMain.js
@@ -1,34 +1,34 @@
 //合并之后的js
 //A模块
-define('moduANme',[],function(require,exports,module){
+define('moduAFunction',[],function(require,exports,module){
 	function moduleA(){
 		alert('我是模块A');
 	}
 	exports.moduleA=moduleA;
 })
 //B的依赖
-define('./moduleC.js',[],function(require,exports,module){
+define('moduBFunction',[],function(require,exports,module){
 	var moduleC=3;
 	exports.moduleC=moduleC;
 })
 //B模块
-define('./js/moduleB.js',['./moduleC.js'],function(require,exports,module){
-	var moduleC=require('./moduleC.js');
+define('moduCFunction',['moduBFunction'],function(require,exports,module){
+	var moduleC=require('moduBFunction').moduleC;
 	function moduleB(){
 		alert('我是模块B');
 	}
 	function alertModuleC(){
-		alert('我是模块c中的c,值为');
+		alert('我是模块c中的c,值为'+moduleC);
 	}	
 	exports.moduleB=moduleB;
 	exports.alertModuleC=alertModuleC;
 })
 
 //C调用以上模块
-seajs.use('moduANme',function(ex){
+seajs.use('moduAFunction',function(ex){
 			ex.moduleA();
 	});
-seajs.use('./js/moduleB.js',function(ex){
+seajs.use('moduCFunction',function(ex){
 			ex.moduleB();
 			ex.alertModuleC();
 });
