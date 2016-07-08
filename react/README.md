911caa001f8d0bdf8a99ffe555ea01a0ebe6a201
tjq <596713230@qq.com>
2016-07-08 20:41:21 +0800

    'hehe'

---
+++ react/README.md
@@ -0,0 +1,1 @@
+# learnfe-react
---
+++ react/webpack.config.js
@@ -0,0 +1,16 @@
+var webpack=require('webpack');
+var path = require('path');
+module.exports = {
+  entry:path.resolve(__dirname, 'app/app.js'),
+  output:{
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module:{
+    loaders: [
+    {test: /\.jsx?$/,loader: 'babel',query: {presets: ['es2015','react']},exclude: /node_modules/},
+    {test: /\.(png|jpg|gif)$/,loader: 'url-loader?limit=8192'},
+    {test: /\.css$/,loader: 'style!css'}
+                ]
+           }
+};
