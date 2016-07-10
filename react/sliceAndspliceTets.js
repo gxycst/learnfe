0e89b1fbe60d5403c4be3d6f4bfbc41a7c16060f
tjq <596713230@qq.com>
2016-07-10 16:32:59 +0800

    '0710'

---
+++ react/sliceAndspliceTets.js
@@ -0,0 +1,8 @@
+var initailArr=[1,2,3];
+var newCopyArr=initailArr.splice();
+var newCopyArr1=initailArr.splice(0,2);
+console.log(newCopyArr);
+console.log(newCopyArr1);
+console.log(initailArr);
+
+
