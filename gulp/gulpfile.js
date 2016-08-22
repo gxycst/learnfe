e0568992400ed3ad4d5ad66d96c9b67db438e852
tjq <596713230@qq.com>
2016-08-22 09:26:22 +0800

    ss

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,22 +1,16 @@
-/*var gulp = require('gulp'),
+    var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload');
     // Styles任务
 gulp.task('styles', function() {
     //编译sass
     return gulp.src('sass/index.scss')
     .pipe(sass())
-    //添加前缀
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     //保存未压缩文件到我们指定的目录下面
     .pipe(gulp.dest(''))
     //给文件添加.min后缀
@@ -26,27 +20,9 @@ gulp.task('styles', function() {
     //输出压缩文件到指定目录
     .pipe(gulp.dest('assets'))
     //提醒任务完成
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(notify({ message: 'gulp任务完成!' }));
 });
-*/
-//包含处理包
-var gulp=require('gulp'),
-    notify=require('gulp-notify'),
-    autoprefixer=require('gulp-autoprefixer'),
-    sass=require('gulp-sass');
-//创建处理任务
 
-gulp.task('cssTask',function(){
-    //用gulp.src找到路径
-    return gulp.src('./scss/index.scss')
-    //编译sass
-    .pipe(sass())
-    //添加浏览器前缀
-    .pipe(autoprefixer('last 2 version','safari 5','ie 8','ie 9','opera 12.1','ios6','android 4'))
-    //用gulp.dest保存未压缩文件到指定目录
-    .pipe(gulp.dest('maxcss'))
-    .pipe(notify({message:'cssTask is ok ^_^!!'}));
-});
 //默认任务
 gulp.task('default', function() {
     gulp.start('cssTask');
