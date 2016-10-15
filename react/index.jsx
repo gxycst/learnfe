7aaf476da1ebf0202d7d90fb508ec423a668701d
tjq <596713230@qq.com>
2016-10-15 12:26:27 +0800

    remove react

--- react/index.jsx
+++
@@ -1,8 +0,0 @@
-var HelloWorld= React.createClass({
-	render: function() {
-		return (
-			<h1>Hello,React!</h1>
-		);
-	}
-});
-ReactDOM.render(<HelloWorld></HelloWorld>,$('#demo')[0]);
--- react/sliceAndspliceTets.js
+++
@@ -1,8 +0,0 @@
-var initailArr=[1,2,3];
-var newCopyArr=initailArr.splice();
-var newCopyArr1=initailArr.splice(0,2);
-console.log(newCopyArr);
-console.log(newCopyArr1);
-console.log(initailArr);
-
-
