0b2ffe8a3db4e3a641441a7810b3a019fdb4f522
fetjq <596713230@qq.com>
2017-02-11 18:37:29 +0800

    S

--- es6/jiegoufuzhi.js
+++ es6/jiegoufuzhi.js
@@ -2,7 +2,7 @@ function test(...values){
 	console.log(Object.prototype.toString.call(values));
 	console.log(...values);
 };
-test(1,2,3);
+test([1,2,3]);
 /*函数的rest参数是一个数组，只能位于最后,函数的length(新参数量)不包含values数.这个参数有点像arguments属性，只不过是一个真数组
 console.log(...[1,2,3]);//扩展运算符,把一个数组的每个元素拎出来。
 ...可以将任何类数组转成数组,和Array.from()类似.也可以将字符串转成数组，如[...'hello']  =>  ['h','e','l','l','o'],
