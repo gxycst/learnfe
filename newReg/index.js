b5641adef0705d097ffa0486734353f9e190744b
taojiangqin <taojiangqin@zbj.com>
2017-06-28 18:01:30 +0800

    .

--- newReg/index.js
+++ newReg/index.js
@@ -1,1 +1,5 @@
 var reg=/^(?!0+(?:\.0+)?$)(?:[1-9]\d*|0)(?:\.\d{1,2})?$/;//必须为正数且最多为两位小数
+console.log(reg.test(0));
+console.log(reg.test(-0.12));
+console.log(reg.test(0.123));
+console.log(reg.test(0.1244));
