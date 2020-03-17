2ec5d034b0d3af72460fd793e3dea80d8ae8e69e
taojiangqin <taojiangqin@tuscloud.io>
2020-03-17 10:20:19 +0800

    learn

---
+++ Vue/element-ui组件自定义传参.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        组件自定义传参,以select为例，如果select写在循环里，触发change事件时可能不只需要传递被选中项的值，还要传递index过去，来改变同一循环中的其他标签的状态。 下面这样写是无效的：
+        <div v-for="(item,index) in itemList">
+            <el-select v-model="item.value" @change="changeStatus(val, index)">
+                <el-option v-for="op in options" :key="op.key" :label="op.label"
+                           :value="op.label"></el-option>
+            </el-select>
+        </div>
+        这样再封装一层就可以了：
+        <div v-for="(item,index) in itemList">
+            <el-select v-model="item.value" @change="(val)=>{changeStatus(val, index)}">
+                <el-option v-for="op in options" :key="op.key" :label="op.label"
+                           :value="op.label"></el-option>
+            </el-select>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                itemList: [],
+                options: []
+            }
+        },
+        methods: {
+            changeStatus(v, i) {
+            }
+        }
+    }
+</script>
---
+++ Vue/异步组件-循环引用.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        异步组件,可解决循环引用等问题
+    </div>
+</template>
+<script>
+    export default {
+        beforeCreate: function () {// 方案一
+            this.$options.components.TreeFolderContents = require('./tree-folder-contents.vue').default
+        },
+        components: {//推荐使用 webpack的异步组件 通过 import 引入,异步组件的注册是一个箭头函数
+            TreeFolderContents: () => import('./tree-folder-contents.vue')
+        },
+    }
+</script>
