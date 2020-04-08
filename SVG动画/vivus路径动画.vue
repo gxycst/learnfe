7775d17a27eca93b6b634f685a983733397be1b7
taojiangqin <taojiangqin@tuscloud.io>
2020-04-08 11:07:25 +0800

    xx

---
+++ SVG动画/vivus路径动画.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div id="my-div"></div>
+    </div>
+</template>
+<script>
+    import Vivus from 'vivus'
+    export default {
+        data() {
+            return {};
+        },
+        mounted() {
+            new Vivus('my-div', {
+                    file: 'http://192.168.103.5:8080/0007.svg',
+                    type: 'oneByOne',
+                }, myVivus => {
+                    // myVivus.play(myVivus.getStatus() === 'end' ? -1 : 1);来回
+                    if (myVivus.getStatus() === 'end') {
+                        myVivus.reset().play();
+                    }
+                }
+            );
+
+        }
+    };
+</script>
+<style lang="less">
+
+</style>
